89113bae88e2b9b9d0ff10f7a04b46db402ab32b
David <david199110@hotmail.com>
2013-10-11 08:57:13 -0700

    small changes

--- src/gui/VPNPanel.java
+++ src/gui/VPNPanel.java
@@ -1,11 +1,17 @@
 package gui;
 
+import javax.swing.JPanel;
+
 /**
  * VPNPanel allows users to choose server mode or client mode,
  * send and receive message, and
  * keep track of the logging
  *
  */
-public class VPNPanel {
+public class VPNPanel extends JPanel {
+	
+	public VPNPanel() {
+		
+	}
 
 }
