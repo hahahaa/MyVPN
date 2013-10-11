6ede525c07cf481d25f7a9799e69528443ff56d4
David <david199110@hotmail.com>
2013-10-11 11:35:49 -0700

    lala

--- src/vpn/MyVPN.java
+++ src/vpn/MyVPN.java
@@ -12,7 +12,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -35,11 +34,11 @@ public class MyVPN {
 	public static JTextField ipField;
 	public static JTextField portField;
 	public static JTextField sendDataField;
-	public static JTextField recvDataField;
 	public static JButton sendButton;
 	public static JButton contButton;
 	public static JButton serverButton;
 	public static JButton clientButton;
+	public static JButton disconnectButton;
 	
 	// TCP components
 	public static ServerSocket hostServer = null;
@@ -49,7 +48,13 @@ public class MyVPN {
 	
 	public static void initPanel() {
 		
-		// set up logArea
+		// set up log panel
+		JPanel logPanel = new JPanel(new BorderLayout());
+		
+		sendDataField = new JTextField(40);
+		sendButton = new JButton("Send");
+		sendButton.setEnabled(false);
+		
 		logArea = new JTextArea(10, 20);
 		logArea.setLineWrap(true);
 		logArea.setWrapStyleWord(true);
@@ -57,43 +62,63 @@ public class MyVPN {
 		logArea.setRows(20);
 		scrollingTextArea = new JScrollPane(logArea);
 		
-		// set up optPanel
+		JPanel sendPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		sendPanel.add(sendDataField);
+		sendPanel.add(sendButton);
+		logPanel.add(sendPanel, BorderLayout.NORTH);
+		logPanel.add(logArea, BorderLayout.CENTER);
+		
+		//-------------------------------------------------------------
+		// set up option panel
+		//-------------------------------------------------------------
 		optPanel = new JPanel(new GridLayout(6, 1));
 		
-		JPanel ipPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		// IP panel
+		JPanel ipPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		ipPanel.add(new JLabel("Host IP:"));
 		ipField = new JTextField(10); 
 		ipField.setText(HOSTIP);
 		ipPanel.add(ipField);
 		optPanel.add(ipPanel);
 		
-		JPanel portPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		// port panel
+		JPanel portPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		portPanel.add(new JLabel("Port:"));
 		portField = new JTextField(10); 
 		portField.setText(DEFAULTPORT);
 		portPanel.add(portField);
 		optPanel.add(portPanel);
 		
+		// disconnect button
+		JPanel buttonPanel3 = new JPanel(new GridLayout(1, 1));
+		disconnectButton = new JButton("Disconnect");
+		disconnectButton.setEnabled(false);
+		buttonPanel3.add(disconnectButton);
+		optPanel.add(buttonPanel3);
+		
+		// server and client button
 		JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
 		serverButton = new JButton("Create server");
-		clientButton = new JButton("Connect as client");
-		
+		clientButton = new JButton("Connect as client");	
 		
 		buttonPanel.add(serverButton);
 		buttonPanel.add(clientButton);
 		optPanel.add(buttonPanel);
 		
+		// continue button
+		JPanel buttonPanel2 = new JPanel(new GridLayout(1, 1));
+		contButton = new JButton("Continue");
+		contButton.setEnabled(false);
+		buttonPanel2.add(contButton);
+		optPanel.add(buttonPanel2);
 		
+		//-------------------------------------------------------------
 		// set up the main panel
+		//-------------------------------------------------------------
 		panel = new JPanel();
 		panel.setLayout(new BorderLayout());
-		panel.add(scrollingTextArea, BorderLayout.CENTER);
+		panel.add(logPanel, BorderLayout.CENTER);
 		panel.add(optPanel, BorderLayout.WEST);
-		
-		
-		
-		
-		
 	}
 	
 	
@@ -104,7 +129,7 @@ public class MyVPN {
 		mainFrame = new JFrame("MyVPN");
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setContentPane(panel);
-		mainFrame.setSize(500, 500);
+		mainFrame.setSize(800, 500);
 		mainFrame.setLocation(100, 100);
 		mainFrame.pack();
 		mainFrame.setVisible(true);	
